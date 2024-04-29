bb9b5cd8a27497af713bd35661f040e5fe557cc5
dolgachcj18 <dolgachcj18@uww.edu>
2024-04-28 21:11:07 -0500

    v12

--- src/App.js
+++ src/App.js
@@ -31,53 +31,53 @@ function App({ signOut }) {
       
   
       <div class="dropdown-header">AL East</div>
-      <a href="/public/teams/orioles.html">Baltimore Orioles</a>
-      <a href="/public/teams/redsox.html">Boston Red Sox</a>
+      <a href="/teams/orioles.html">Baltimore Orioles</a>
+      <a href="/teams/redsox.html">Boston Red Sox</a>
       <a href="/public/teams/yankees.html">New York Yankees</a>
-      <a href="/public/teams/rays.html">Tampa Bay Rays</a>
+      <a href="/teams/rays.html">Tampa Bay Rays</a>
       <a href="/teams/bluejays.html">Toronto Blue Jays</a>
       
  
       <div class="dropdown-header">AL Central</div>
-      <a href="/public/teams/whitesox.html">Chicago White Sox</a>
-      <a href="/public/teams/guardians.html">Cleveland Guardians</a>
-      <a href="/public/teams/tigers.html">Detroit Tigers</a>
-      <a href="/public/teams/royals.html">Kansas City Royals</a>
-      <a href="/public/teams/twins.html">Minnesota Twins</a>
+      <a href="/teams/whitesox.html">Chicago White Sox</a>
+      <a href="/teams/guardians.html">Cleveland Guardians</a>
+      <a href="/teams/tigers.html">Detroit Tigers</a>
+      <a href="/teams/royals.html">Kansas City Royals</a>
+      <a href="/teams/twins.html">Minnesota Twins</a>
       
      
       <div class="dropdown-header">AL West</div>
-      <a href="/public/teams/astros.html">Houston Astros</a>
-      <a href="/public/teams/angels.html">Los Angeles Angels</a>
-      <a href="/public/teams/athletics.html">Oakland Athletics</a>
-      <a href="/public/teams/mariners.html">Seattle Mariners</a>
-      <a href="/public/teams/rangers.html">Texas Rangers</a>
+      <a href="/teams/astros.html">Houston Astros</a>
+      <a href="/teams/angels.html">Los Angeles Angels</a>
+      <a href="/teams/athletics.html">Oakland Athletics</a>
+      <a href="/teams/mariners.html">Seattle Mariners</a>
+      <a href="/teams/rangers.html">Texas Rangers</a>
  
       <div class="dropdown-divider">National League</div>
       
 
       <div class="dropdown-header">NL East</div>
-      <a href="/public/teams/braves.html">Atlanta Braves</a>
-      <a href="/public/teams/marlins.html">Miami Marlins</a>
-      <a href="/public/teams/mets.html">New York Mets</a>
-      <a href="/public/teams/phillies.html">Philadelphia Phillies</a>
-      <a href="/public/teams/nationals.html">Washington Nationals</a>
+      <a href="/teams/braves.html">Atlanta Braves</a>
+      <a href="/teams/marlins.html">Miami Marlins</a>
+      <a href="/teams/mets.html">New York Mets</a>
+      <a href="/teams/phillies.html">Philadelphia Phillies</a>
+      <a href="/teams/nationals.html">Washington Nationals</a>
       
    
       <div class="dropdown-header">NL Central</div>
-      <a href="/public/teams/cubs.html">Chicago Cubs</a>
-      <a href="/public/teams/reds.html">Cincinnati Reds</a>
-      <a href="/public/teams/brewers.html">Milwaukee Brewers</a>
-      <a href="/public/teams/pirates.html">Pittsburgh Pirates</a>
-      <a href="/public/teams/cardinals.html">St. Louis Cardinals</a>
+      <a href="/teams/cubs.html">Chicago Cubs</a>
+      <a href="/teams/reds.html">Cincinnati Reds</a>
+      <a href="/teams/brewers.html">Milwaukee Brewers</a>
+      <a href="/teams/pirates.html">Pittsburgh Pirates</a>
+      <a href="/teams/cardinals.html">St. Louis Cardinals</a>
       
   
       <div class="dropdown-header">NL West</div>
-      <a href="/public/teams/diamondbacks.html">Arizona Diamondbacks</a>
-      <a href="/public/teams/rockies.html">Colorado Rockies</a>
-      <a href="/public/teams/dodgers.html">Los Angeles Dodgers</a>
-      <a href="/public/teams/padres.html">San Diego Padres</a>
-      <a href="/public/teams/giants.html">San Francisco Giants</a>
+      <a href="/teams/diamondbacks.html">Arizona Diamondbacks</a>
+      <a href="/teams/rockies.html">Colorado Rockies</a>
+      <a href="/teams/dodgers.html">Los Angeles Dodgers</a>
+      <a href="/teams/padres.html">San Diego Padres</a>
+      <a href="/teams/giants.html">San Francisco Giants</a>
     </div>
        </div>
       </div>
